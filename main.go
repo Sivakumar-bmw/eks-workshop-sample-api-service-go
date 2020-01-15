42eabcb2b57ab2705212c58668ac4426f7de9acc
Sivakumar-bmw <59935271+Sivakumar-bmw@users.noreply.github.com>
2020-01-15 15:10:24 -0500

    3rd commit

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - Siva Natarajan"}
+		res := &response{Message: "Hello World - Siva and Sanjeev"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
